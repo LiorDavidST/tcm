c34b859741886cb176090f271a563d09a993e7ed
LiorDavidST <lst.lior@gmail.com>
2025-04-30 14:26:29 +0300

    Add files via upload

--- Python_Lessons/app.py
+++ Python_Lessons/app.py
@@ -1,19 +1,29 @@
 from flask import Flask, request, jsonify, render_template
 from runner import run_python_code
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
-# טוען את index.html מתוך תיקיית templates
+
+# 🔹 דף הבית – שער ל־TCM ACADEMY (academy.html)
 @app.route('/')
-def index():
+def home():
+    return render_template('academy.html')
+
+
+# 🔹 דף הקורס האינטראקטיבי – קבצי קורס Python
+@app.route('/python')
+def python_course():
     return render_template('index.html')
 
+
+# 🔹 API להרצת קוד Python
 @app.route('/run', methods=['POST'])
 def run_code():
     data = request.get_json()
     code = data.get('code', '')
     output = run_python_code(code)
     return jsonify({'output': output})
+
 
 if __name__ == '__main__':
     app.run(debug=True)
