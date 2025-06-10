ae7fff0ab0938f3c85e1d66fc0f28c642e1c9855
LiorDavidST <lst.lior@gmail.com>
2025-06-10 21:37:18 +0300

    Delete test_db directory

--- test_db/add_course_test.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from firebase.course_management import add_course
-
-# הוספת קורס חדש
-add_course('course_1', 'Python for Beginners', 'An introductory course to Python', 100, 30)
--- test_db/add_user_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-# הוספת תיקיית הפרויקט לשביל המודולים
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from firebase.user_management import add_user
-
-add_user('12345', 'John', 'Doe', 'johndoe@example.com')
